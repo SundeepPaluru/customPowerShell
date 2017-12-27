44fedbda2b0fd195a47adf2a3570b011b80cbec0
SunDeep <p.sundeep@live.com>
2017-12-28 00:28:01 +0530

    All Ps Custom Scripts

---
+++ Scripts/Az-CustChecks.ps1
@@ -0,0 +1,8 @@
+####################################
+# Name: SundeepPaluru
+# E-Mail: Sundeep.Paluru@gmail.com
+#   WwW.TheSunDeep.Com
+# {Script Your Own Destiny}
+####################################
+
+Write-Host "Welcome to MyWorld..."
